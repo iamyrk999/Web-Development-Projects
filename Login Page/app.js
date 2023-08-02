7676dfe2378b5bad41687f3aa4ef8c387a72e36b
iamyrk999 <140267646+iamyrk999@users.noreply.github.com>
2023-08-02 11:39:40 +0530

    Add files via upload
    Basic Login form and Sign Up from using HTML5,CSS3,JavaScript

---
+++ Login Page/app.js
@@ -0,0 +1,16 @@
+const title = document.getElementById('title');
+const nameField = document.getElementById('nameField');
+const signinbtn = document.getElementById('signinbtn');
+const signupbtn = document.getElementById('signupbtn');
+
+
+
+signupbtn.onclick = function () {
+  title.innerHTML = "Sign Up";
+  nameField.style.display = "block";
+}
+
+signinbtn.onclick = function () {
+  title.innerHTML = "Log In";
+  nameField.style.display = "none";
+}
